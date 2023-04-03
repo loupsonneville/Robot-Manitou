ef7931f156b648c90f1cb7f7df1f8c468ed51dad
loupsonneville <loup.sonneville@stfelixlasalle.fr>
2023-04-03 15:01:51 +0200

    up mom

--- Loup SONNEVILLE/automation studio/BDD/Temp/Includes/MDatabase/Variablesvar.h
+++ Loup SONNEVILLE/automation studio/BDD/Temp/Includes/MDatabase/Variablesvar.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _BUR_1680093383_1_
-#define _BUR_1680093383_1_
+#ifndef _BUR_1680525746_1_
+#define _BUR_1680525746_1_
 
 #include <bur/plctypes.h>
 
@@ -13,7 +13,7 @@
 
 
 /* Variables */
-_BUR_LOCAL unsigned char MpDatabaseCore_0;
+_BUR_LOCAL struct MpDatabaseCoreInfoType MpDatabaseCore_0;
 
 
 
@@ -29,5 +29,5 @@ __asm__(".ascii \"iecfile \\\"Logical/MDatabase/Variables.var\\\" scope \\\"loca
 __asm__(".previous");
 
 
-#endif /* _BUR_1680093383_1_ */
+#endif /* _BUR_1680525746_1_ */
 
--- Loup SONNEVILLE/automation studio/BDD/Temp/Objects/Config1/X90CP174_24_00/Libraries.h
+++ Loup SONNEVILLE/automation studio/BDD/Temp/Objects/Config1/X90CP174_24_00/Libraries.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _LIBRARIES_20230330134534_
-#define _LIBRARIES_20230330134534_
+#ifndef _LIBRARIES_20230403144226_
+#define _LIBRARIES_20230403144226_
 
 __asm__(".section \".plc\"");
 
@@ -498,5 +498,5 @@ __asm__(".ascii \"iecfile \\\"Logical/AsHttp/AsHttp.var\\\" scope \\\"global\\\"
 __asm__(".previous");
 
 
-#endif /* _LIBRARIES_20230330134534_ */
+#endif /* _LIBRARIES_20230403144226_ */
 
