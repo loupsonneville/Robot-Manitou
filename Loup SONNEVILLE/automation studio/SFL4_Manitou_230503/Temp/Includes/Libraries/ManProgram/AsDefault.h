7e206e2e568608f25f908fef6e0332dbf11457e1
loupsonneville <loup.sonneville@stfelixlasalle.fr>
2023-05-04 15:20:41 +0200

    up capture

---
+++ Loup SONNEVILLE/automation studio/SFL4_Manitou_230503/Temp/Includes/Libraries/ManProgram/AsDefault.h
@@ -0,0 +1,8 @@
+#ifndef _LIBRARIES_MANPROGRAM_DEFAULT_1850953433
+#define _LIBRARIES_MANPROGRAM_DEFAULT_1850953433
+#include "../AsDefault.h"
+#include <bur/plctypes.h>
+#include <bur/plc.h>
+#include <typesTYP.h>
+#include <variablesVAR.h>
+#endif
---
+++ Loup SONNEVILLE/automation studio/SFL4_Manitou_230503/Temp/Objects/Config1/X90CP174_24_00/ManProgram/Init.ld.c
@@ -0,0 +1,41 @@
+#define _DEFAULT_INCLUDE
+#include <bur\plctypes.h>
+#include "C:/Users/loup.sonneville/Documents/Github/SN2_SFL4Manitou_23/Loup SONNEVILLE/automation studio/SFL4_Manitou_230503/Temp/Objects/Config1/X90CP174_24_00/ManProgram/Initld.h"
+#line 1 "C:/Users/loup.sonneville/Documents/Github/SN2_SFL4Manitou_23/Loup SONNEVILLE/automation studio/SFL4_Manitou_230503/Logical/Libraries/ManProgram/Init.nodebug"
+#line 1 "C:/Users/loup.sonneville/Documents/Github/SN2_SFL4Manitou_23/Loup SONNEVILLE/automation studio/SFL4_Manitou_230503/Logical/Libraries/ManProgram/Init.ld"
+void __BUR__ENTRY_INIT_FUNCT__(void){{}
+}
+#line 1 "C:/Users/loup.sonneville/Documents/Github/SN2_SFL4Manitou_23/Loup SONNEVILLE/automation studio/SFL4_Manitou_230503/Logical/Libraries/ManProgram/Init.nodebug"
+
+void __AS__ImplInitInit_ld(void){__BUR__ENTRY_INIT_FUNCT__();}
+
+__asm__(".section \".plc\"");
+__asm__(".ascii \"iecfile \\\"Logical/Global.typ\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/operator/operator.typ\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/runtime/runtime.typ\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/astime/astime.typ\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/AsIecCon/AsIecCon.typ\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/MpDatabase/MpDatabase.typ\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/MpDatabase/MpDatabaseError.typ\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/MpBase/MpBase.typ\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/AsHttp/AsHttp.typ\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/operator/operator.fun\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/runtime/runtime.fun\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/astime/astime.fun\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/AsIecCon/AsIecCon.fun\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/MpDatabase/MpDatabase.fun\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/MpBase/MpBase.fun\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/AsHttp/AsHttp.fun\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Global.var\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Temp/Includes/AS_TempDecl/Config1/GlobalComponents/MpComponents.var\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/operator/operator.var\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/runtime/runtime.var\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/astime/astime.var\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/AsIecCon/AsIecCon.var\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/MpBase/MpBase.var\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/AsHttp/AsHttp.var\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/ManProgram/Types.typ\\\" scope \\\"local\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/ManProgram/Variables.var\\\" scope \\\"local\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"C:/Users/loup.sonneville/Documents/Github/SN2_SFL4Manitou_23/Loup SONNEVILLE/automation studio/SFL4_Manitou_230503/Temp/Objects/Config1/X90CP174_24_00/ManProgram/Init.ld.var\\\" scope \\\"local\\\"\\n\"");
+__asm__(".ascii \"plcreplace \\\"C:/Users/loup.sonneville/Documents/Github/SN2_SFL4Manitou_23/Loup SONNEVILLE/automation studio/SFL4_Manitou_230503/Temp/Objects/Config1/X90CP174_24_00/ManProgram/Init.ld.c\\\" \\\"C:/Users/loup.sonneville/Documents/Github/SN2_SFL4Manitou_23/Loup SONNEVILLE/automation studio/SFL4_Manitou_230503/Logical/Libraries/ManProgram/Init.ld\\\"\\n\"");
+__asm__(".previous");
